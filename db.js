2fe8b3fed10a456106ffc8e0c7722ba015532c5e
von Buelow <jaspar@UMICH.EDU>
2020-06-18 22:18:21 -0400

    Database connection

--- db.js
+++ db.js
@@ -1,2 +1,19 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "postgresql://postgres:41361@localhost/biztime_test";
+} else {
+  DB_URI = "postgresql://postgres:41361@localhost/biztime";
+}
+
+let db = new Client({
+  connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
