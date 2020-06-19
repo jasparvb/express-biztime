d244ac9df7cf339fcfbd8762708da1ada56fd6f3
von Buelow <jaspar@UMICH.EDU>
2020-06-19 17:15:03 -0400

    Invoices GET routes

--- routes/invoices.js
+++ routes/invoices.js
@@ -13,5 +13,37 @@ router.get('', async function (req, res, next) {
     }
 });
 
+router.get('/:id', async function (req, res, next) {
+    try {
+        let id = req.params.id;
+        const result = await db.query(
+            `SELECT id, amt, paid, add_date, paid_date, comp_code
+            FROM invoices 
+            WHERE id = $1`, 
+            [id]
+        );
+        const company = await db.query(
+            `SELECT code, name, description 
+            FROM companies 
+            WHERE code = $1`, 
+            [result.rows[0].comp_code]
+        );
+        if (result.rows.length === 0){
+            throw new ExpressError("No invoice found with that id", 404);
+        }
+        const r = result.rows[0];
+        const invoice = {
+            id: r.id,
+            amt: r.amt,
+            paid: r.paid,
+            add_date: r.add_date,
+            paid_date: r.paid_date,
+            company: company.rows[0]
+        };
+        return res.json({invoice});
+    } catch (e) {
+        return next(e);
+    }
+});
 
 module.exports = router;
