d20fc1f1d929d4d563af52ef13a54effee20c91f
von Buelow <jaspar@UMICH.EDU>
2020-06-19 16:33:15 -0400

    Companies PUT and DELETE routes

--- routes/companies.js
+++ routes/companies.js
@@ -49,20 +49,39 @@ router.post('', async function (req, res, next) {
 router.put('/:code', async function (req, res, next) {
     try {
         let code = req.params.code;
+        const { name, description } = req.body;
         const result = await db.query(
-            `SELECT code, name, description 
-            FROM companies 
-            WHERE code = $1`, 
-            [code]
+            `UPDATE companies 
+            SET name=$1, description=$2 
+            WHERE code = $3 
+            RETURNING code, name, description`, 
+            [name, description, code]
         );
         if (result.rows.length === 0){
             throw new ExpressError("No company found with that code", 404);
         }
-        return res.json({company: result.rows});
+        return res.json({company: result.rows[0]});
     } catch (e) {
         return next(e);
     }
 });
 
+router.delete("/:code", async function (req, res, next) {
+    try {
+        let code = req.params.code;
+        const result = await db.query(
+            `DELETE FROM companies 
+            WHERE code = $1
+            RETURNING code`,
+            [code]
+        );
+        if (result.rows.length === 0){
+            throw new ExpressError("No company found with that code", 404);
+        }
+        return res.json({status: "Deleted"});
+    } catch (e) {
+        return next(e);
+    }
+});
 
 module.exports = router;
